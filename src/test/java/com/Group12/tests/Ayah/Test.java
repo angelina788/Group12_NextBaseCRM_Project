4383b90c56695973b0d149f4781701510c9d114f
Ayah N <ayah.noor0213@gmail.com>
2021-01-17 18:31:31 -0500

    test2

--- src/test/java/com/Group12/tests/Ayah/Test.java
+++ src/test/java/com/Group12/tests/Ayah/Test.java
@@ -2,7 +2,10 @@ package com.Group12.tests.Ayah;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("hello");
         System.out.println("hello test test");
+        System.out.println("Test2");
+
     }
 }
