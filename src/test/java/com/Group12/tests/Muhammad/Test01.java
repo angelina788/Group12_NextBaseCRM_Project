c3a1a8eda0a50db591ab7298c559618e9c0c8f9a
Muhammad-54107 <Muhammad.qureshi107@gmail.com>
2021-01-18 19:18:49 -0500

    New branch test1

--- src/test/java/com/Group12/tests/Muhammad/Test01.java
+++ src/test/java/com/Group12/tests/Muhammad/Test01.java
@@ -4,6 +4,8 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hi, My name is Muhammad");
 
+        System.out.println("Testing Testing");
+
 
     }
 }
