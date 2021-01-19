c00b6cf61a7bd84eac20965c0d6bba4236e63a32
volkan <vsaydogan@gmail.com>
2021-01-18 20:10:54 -0500

    individual test3

--- src/test/java/com/Group12/tests/Volkan/test1.java
+++ src/test/java/com/Group12/tests/Volkan/test1.java
@@ -3,5 +3,6 @@ package com.Group12.tests.Volkan;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("check up date");
     }
 }
