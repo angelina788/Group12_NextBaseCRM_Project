7312ee11ff12093e89ea9247be8aef318d19552e
volkan <vsaydogan@gmail.com>
2021-01-28 11:27:42 -0500

    individual test4

--- src/test/java/com/Group12/tests/Volkan/test1.java
+++ src/test/java/com/Group12/tests/Volkan/test1.java
@@ -4,5 +4,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("check up date");
+        System.out.println("Guys Can you See Me?");
     }
 }
