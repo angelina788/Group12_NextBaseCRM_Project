e385f91d8611166db924fb752eedcc3bb857fa5b
Muhammad-54107 <Muhammad.qureshi107@gmail.com>
2021-01-28 11:57:53 -0500

    new Crm push

--- src/test/java/com/Group12/tests/Muhammad/TCool1_Muhammad.java
+++ src/test/java/com/Group12/tests/Muhammad/TCool1_Muhammad.java
@@ -1,4 +1,15 @@
 package com.Group12.tests.Muhammad;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
+import javax.xml.transform.Source;
+import java.sql.SQLOutput;
+
 public class TCool1_Muhammad {
+
+    public static void main(String[] args) {
+        System.out.println("Git Hub Practice ");
+
+    }
+
 }
